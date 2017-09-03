9c82a69fbbd80799fec51790efae1e21bc9487d1
Sam Ellenrieder <sam.ell@web.de>
2017-09-03 14:49:57 +0100

    Bowling-challenge, approaching tests

--- jasmine-standalone-2.8.0/spec/bowlingSpec.js
+++ jasmine-standalone-2.8.0/spec/bowlingSpec.js
@@ -6,10 +6,18 @@ describe("Bowling", function() {
     });
    
    describe("Total points are zero by default", function() {
-    it('Total: 0', function()  {
-      expect(bowling.total).toEqual(0);
+     it('Total: 0', function()  {
+       expect(bowling.total).toEqual(0);
+     });
     });
-   });
+     
+    describe("Adding knocked pins/points to total", function() {
+      it('changes total', function() {
+        bowling.points(5)
+        bowling.points(2)
+        expect(bowling.total).toEqual(7)
+      });
+    });
 
 
 });
--- jasmine-standalone-2.8.0/src/bowling.js
+++ jasmine-standalone-2.8.0/src/bowling.js
@@ -1,8 +1,15 @@
 function Bowling(){
-  this.total = 0;
+  this.total = 0
 
 };
 
+Bowling.prototype.points = function(value){
+  if (this.points === 10) {
+    this.total += value;
+  } else {
+    this.total += value;
+  };
+};
 
 
 
