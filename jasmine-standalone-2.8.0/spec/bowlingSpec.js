e53389c6bbc1d6932f9b2bba88ca93250b332fb1
Sam Ellenrieder <sam.ell@web.de>
2017-09-03 18:13:14 +0100

    Bowling-challenge, second test, HTML upgrade

--- jasmine-standalone-2.8.0/spec/bowlingSpec.js
+++ jasmine-standalone-2.8.0/spec/bowlingSpec.js
@@ -3,6 +3,7 @@ describe("Bowling", function() {
   var bowling;
     beforeEach(function(){
       bowling = new Bowling();
+      bowling2 = new Bowling();
     });
    
    describe("Total points are zero by default", function() {
@@ -13,15 +14,72 @@ describe("Bowling", function() {
      
     describe("Adding knocked pins/points to total", function() {
       it('changes total', function() {
-        bowling.points(5)
-        bowling.points(2)
-        expect(bowling.total).toEqual(7)
+        bowling.throw1(10)
+        expect(bowling.total).toEqual(10)
       });
     });
+    
+    describe("Throw 2 is 0 if throw 1 is strike", function() {
+      it('Throw2 equals 0',function() {
+        bowling.throw1(10)
+        bowling.throw2(0)
+        expect(bowling.total).toEqual(10)
+      });
+    });
+
+    
 
 
 
 
 });
 
-  
+  // describe("temperature is 20 by default", function() {
+  //   it('20 degrees', function() {
+  //     expect(thermostat.temperature).toEqual(20);
+  //   });
+  // });
+
+  //   it('raises temperature by 5 degrees', function() {
+  //     thermostat.up(5)
+  //     expect(thermostat.temperature).toEqual(25);
+  //   });
+
+  //   it("lowers temperature by 5 degrees", function() {
+  //     thermostat.down(5)
+  //     expect(thermostat.temperature).toEqual(15);
+  //   });
+
+  //   it("temperature cant be decreased lower than 10", function() {
+  //     expect(function() {thermostat.down(11)}).toThrow('cannot decrease temperature lower than min');
+  //   });
+
+  //   it("25 is max with powersavingmode", function() {
+  //     expect(function() {thermostat.up(6)}).toThrow('You cannot go over 25 in PSM!');
+  //   });
+
+  //   it("32 is max without powersavingmode", function() {
+  //     thermostat2.powersavingmode = false;
+  //     expect(function() {thermostat2.up(13)}).toThrow('You cannot go over 32!');
+  //   });
+
+  //   it("resets to 20 with reset", function() {
+  //     thermostat.reset();
+  //     expect(thermostat.temperature).toEqual(20);
+  //   });
+
+  //   it("tells you energy usage low", function() {
+  //     thermostat.down(3);
+  //     expect(thermostat.usage()).toEqual('low');
+  //   });
+
+  //   it("tells energy usage mid", function() {
+  //     expect(thermostat2.usage()).toEqual('mid');
+  //   });
+
+  //   it("tells energy usage high", function() {
+  //     thermostat3.powersavingmode = false;
+  //     thermostat3.up(6);
+  //     expect(thermostat3.usage()).toEqual('high');
+  //   });
+  // });
--- jasmine-standalone-2.8.0/src/bowling.js
+++ jasmine-standalone-2.8.0/src/bowling.js
@@ -1,13 +1,21 @@
 function Bowling(){
   this.total = 0
-
+  
 };
 
-Bowling.prototype.points = function(value){
-  if (this.points === 10) {
-    this.total += value;
+Bowling.prototype.throw1 = function(value1){
+  if (this.throw1 === 10) {
+    this.total += 10;
   } else {
-    this.total += value;
+    this.total += value1;
+  };
+};
+
+Bowling.prototype.throw2 = function(value2){
+  if (this.throw1 === 10) {
+    this.total === 10
+  } else {
+    this.total += value2;
   };
 };
 
@@ -21,4 +29,47 @@ Bowling.prototype.points = function(value){
 
 
 
+// function Thermostat(){
+//   this.temperature = 20;
+//   this.powersavingmode = true;
+// };
 
+// Thermostat.prototype.up = function(value){
+//   this.temperature += value;
+// };
+
+// Thermostat.prototype.down = function(value){
+//   if (this.temperature - value < 10) {
+//     throw 'cannot decrease temperature lower than min';
+//   } else {
+//     this.temperature -= value;
+//   };
+// };
+
+
+// Thermostat.prototype.up = function(value){
+//   if (this.powersavingmode === true && this.temperature + value > 25) {
+//     throw('You cannot go over 25 in PSM!');
+//   }
+//   else if (this.powersavingmode === false && this.temperature + value > 32) {
+//     throw('You cannot go over 32!');
+//   }
+//   else {
+//     this.temperature += value;
+//   };
+// };
+//   Thermostat.prototype.reset = function(){
+//     this.temperature = 20;
+//   };
+
+//   Thermostat.prototype.usage = function(){
+//     if (this.temperature  < 18) {
+//       return("low");
+//     }
+//     else if  (this.temperature < 25 && this.temperature > 18) {
+//       return("mid");
+//     }
+//     else {
+//       return("high");
+//     };
+//   };
