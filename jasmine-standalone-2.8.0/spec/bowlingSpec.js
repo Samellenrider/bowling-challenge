c6202297ef58680512f146aaf23c01788436f8f2
Sam Ellenrieder <sam.ell@web.de>
2017-09-09 16:50:14 +0100

    next test

--- jasmine-standalone-2.8.0/spec/bowlingSpec.js
+++ jasmine-standalone-2.8.0/spec/bowlingSpec.js
@@ -2,49 +2,105 @@ describe("Bowling", function() {
 
   var bowling;
     beforeEach(function(){
-      bowling = new Bowling();
-      //bowling2 = new Bowling();
+      bowling1 = new Bowling();
+      bowling2 = new Bowling();
+      bowling3 = new Bowling();
     });
-   
+
    describe("Total points are zero by default", function() {
      it('Total: 0', function()  {
-       expect(bowling.total).toEqual(0);
+       expect(bowling1.total).toEqual(0);
      });
     });
-     
+
     describe("Adding knocked pins/points to total", function() {
       it('changes total', function() {
-        bowling.throw1(10)
-        expect(bowling.total).toEqual(10)
+        bowling1.throw1(10)
+        expect(bowling1.total).toEqual(10)
       });
     });
-    
+
     describe("Throw 2 is 0 if throw 1 is strike", function() {
       it('Throw2 equals 0', function() {
-        bowling.throw1(10)
-        bowling.throw2(0)
-        expect(bowling.total).toEqual(10)
+        bowling1.throw1(10)
+        bowling1.throw2(0)
+        expect(bowling1.total).toEqual(10)
       });
     });
 
     describe("Score of one round can't be over 10", function() {
       it('Throws error', function() {
-        bowling.throw1(7)
-      expect(function() {bowling.throw2(4)}).toThrow("Total of one round can't be over 10 points");
+        bowling1.throw1(7)
+        expect(function() {bowling1.throw2(4)}).toThrow("Total of one round can't be over 10 points");
       });
     });
-    
-    // describe("Throw2 is in range (10 - throw", function() {
-    //   it('leaves only possible points', function(){
-    //     bowling.throw1(7)
-    //     expect(bowling.throw2).toBe(=<3)
-    //   });
-    // });
-    
 
+    describe("Throw2 is in range (10 - throw", function() {
+      it('leaves only possible points', function(){
+        bowling1.throw1(7)
+        expect(function() {bowling1.throw2(3)}).not.toThrow("Total of one round can't be over 10 points");
+      });
+    });
+
+    describe("Throw 1 is a strike", function() {
+      it('adds second and third throw to bowling1.total', function() {
+        bowling1.throw1(10)
+        bowling2.throw1(6)
+        bowling2.throw2(2)
+        expect(bowling1.total).toEqual(18)
+      });
+    });
 
 
 
 });
 
-  
+  // describe("temperature is 20 by default", function() {
+  //   it('20 degrees', function() {
+  //     expect(thermostat.temperature).toEqual(20);
+  //   });
+  // });
+
+  //   it('raises temperature by 5 degrees', function() {
+  //     thermostat.up(5)
+  //     expect(thermostat.temperature).toEqual(25);
+  //   });
+
+  //   it("lowers temperature by 5 degrees", function() {
+  //     thermostat.down(5)
+  //     expect(thermostat.temperature).toEqual(15);
+  //   });
+
+  //   it("temperature cant be decreased lower than 10", function() {
+  //     expect(function() {thermostat.down(11)}).toThrow('cannot decrease temperature lower than min');
+  //   });
+
+  //   it("25 is max with powersavingmode", function() {
+  //     expect(function() {thermostat.up(6)}).toThrow('You cannot go over 25 in PSM!');
+  //   });
+
+  //   it("32 is max without powersavingmode", function() {
+  //     thermostat2.powersavingmode = false;
+  //     expect(function() {thermostat2.up(13)}).toThrow('You cannot go over 32!');
+  //   });
+
+  //   it("resets to 20 with reset", function() {
+  //     thermostat.reset();
+  //     expect(thermostat.temperature).toEqual(20);
+  //   });
+
+  //   it("tells you energy usage low", function() {
+  //     thermostat.down(3);
+  //     expect(thermostat.usage()).toEqual('low');
+  //   });
+
+  //   it("tells energy usage mid", function() {
+  //     expect(thermostat2.usage()).toEqual('mid');
+  //   });
+
+  //   it("tells energy usage high", function() {
+  //     thermostat3.powersavingmode = false;
+  //     thermostat3.up(6);
+  //     expect(thermostat3.usage()).toEqual('high');
+  //   });
+  // });
--- jasmine-standalone-2.8.0/src/bowling.js
+++ jasmine-standalone-2.8.0/src/bowling.js
@@ -32,3 +32,47 @@ Bowling.prototype.throw2 = function(value2){
 
 
 
+// function Thermostat(){
+//   this.temperature = 20;
+//   this.powersavingmode = true;
+// };
+
+// Thermostat.prototype.up = function(value){
+//   this.temperature += value;
+// };
+
+// Thermostat.prototype.down = function(value){
+//   if (this.temperature - value < 10) {
+//     throw 'cannot decrease temperature lower than min';
+//   } else {
+//     this.temperature -= value;
+//   };
+// };
+
+
+// Thermostat.prototype.up = function(value){
+//   if (this.powersavingmode === true && this.temperature + value > 25) {
+//     throw('You cannot go over 25 in PSM!');
+//   }
+//   else if (this.powersavingmode === false && this.temperature + value > 32) {
+//     throw('You cannot go over 32!');
+//   }
+//   else {
+//     this.temperature += value;
+//   };
+// };
+//   Thermostat.prototype.reset = function(){
+//     this.temperature = 20;
+//   };
+
+//   Thermostat.prototype.usage = function(){
+//     if (this.temperature  < 18) {
+//       return("low");
+//     }
+//     else if  (this.temperature < 25 && this.temperature > 18) {
+//       return("mid");
+//     }
+//     else {
+//       return("high");
+//     };
+//   };
